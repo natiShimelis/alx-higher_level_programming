4f08d4c72b608e58667343e23d1a1a1faaa6b432
Nathnael Shimelis <natishimelisg@gmail.com>
2024-01-03 12:10:19 -0800

    fix_6

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
-        language that combines remarkable power with very clear syntax"
-str = str[39:67] + str[107:112] + str[:6]
-print(str)
+str_val = "Python is an interpreted, interactive, object-oriented programming\
+language that combines remarkable power with very clear syntax"
+str_val = str_val[39:67] + str_val[107:112] + str_val[:6]
+print(str_val)
